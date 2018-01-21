3bae98b88c15e2afc75b9956a30a68c052a330b1
neal1991 <neal1991@sina.com>
2018-01-21 22:04:43 +0800

    fork from topsup

--- main.py
+++ main.py
@@ -1,35 +1,65 @@
-import speech_recognition as sr
+# -*- coding: utf-8 -*-
+
+# @Author  : Skye
+# @Time    : 2018/1/8 20:38
+# @desc    : 答题闯关辅助，截屏 ，OCR 识别，百度搜索
+
+
+from PIL import Image
+import screenshot, ocr, methods
+from threading import Thread
 import time
+import configparser
 
-APP_ID = '10082951'
-APP_KEY = 'qMuA8fTWTM4AwtdP4uGa0ZHqzGOj18LP'
-SECRET_KEY = 'VjdnmhW6OuEdKUIegEwvqev5fHipAtQZ'
+# 读取配置文件
+config = configparser.ConfigParser()
+config.read('./config/configure.conf', encoding='utf-8')
 
-def get_file_content(filepath):
-    with open(filepath, 'rb') as f:
-        return f.read()
+while True:
+    # 截图
+    t = time.clock()
+    screenshot.check_screenshot()
 
-def get_question():
-    # api_speech = AipSpeech(APP_ID, APP_KEY, SECRET_KEY)
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print('say something')
-        time2 = time.time()
-        audio = r.listen(source)
-        print('listen:' + str(time.time() - time2))
-    # with open('result.wav', 'wb') as f:
-    #     f.write(audio.get_raw_data())
-    # print(api_speech.asr(audio.get_raw_data(), 'wav', 16000, {
-    #     'lan': 'zh'
-    # }))
+    # end_time = time.clock()
+    # print(end_time - t)
 
-    try:
-        print('you said:' + r.recognize_sphinx(audio, language="zh-CN"))
-    except Exception as e:
-        print(e)
+    img = Image.open("./screenshot.png")
 
+    # 文字识别,可选 Tesseract 和 Baidu ,请在 config/configure.conf 中进行相应配置
 
-if __name__ == '__main__':
-    time1 = time.time()
-    get_question()
-    print(time.time() - time1)
+    # ocr_img: 需要分别截取题目和选项区域，使用 Tesseract
+    # ocr_img_tess： 题目和选项一起截，使用 Tesseract
+    # ocr_img_baidu： 题目和选项一起截，使用 baidu ocr，需配置 key
+
+    # question, choices = ocr.ocr_img(img, config)
+    question, choices = ocr.ocr_img_tess(img, config)
+    # question, choices = ocr.ocr_img_baidu(img, config)
+
+    # end_time2 = time.clock()
+    # print(end_time2 - end_time)
+
+    # 用不同方法输出结果，取消某个方法在前面加上#
+
+    # # 打开浏览器方法搜索问题
+    # methods.run_algorithm(0, question, choices)
+    # # 将问题与选项一起搜索方法，并获取搜索到的结果数目
+    # methods.run_algorithm(1, question, choices)
+    # # 用选项在问题页面中计数出现词频方法
+    # methods.run_algorithm(2, question, choices)
+
+    # 多线程
+    m1 = Thread(methods.run_algorithm(0, question, choices))
+    m2 = Thread(methods.run_algorithm(1, question, choices))
+    m3 = Thread(methods.run_algorithm(2, question, choices))
+    m1.start()
+    m2.start()
+    m3.start()
+
+    end_time3 = time.clock()
+    print('用时: {0}'.format(end_time3 - t))
+
+    go = input('输入回车继续运行,输入 n 回车结束运行: ')
+    if go == 'n':
+        break
+
+    print('------------------------')
---
+++ methods.py
@@ -0,0 +1,84 @@
+import requests
+import webbrowser
+import urllib.parse
+
+# # 颜色兼容Win 10
+from colorama import init,Fore
+init()
+
+def open_webbrowser(question):
+    webbrowser.open('https://baidu.com/s?wd=' + urllib.parse.quote(question))
+
+def open_webbrowser_count(question,choices):
+    print('\n-- 方法2： 题目+选项搜索结果计数法 --\n')
+    print('Question: ' + question)
+    if '不是' in question:
+        print('**请注意此题为否定题,选计数最少的**')
+
+    counts = []
+    for i in range(len(choices)):
+        # 请求
+        req = requests.get(url='http://www.baidu.com/s', params={'wd': question + choices[i]})
+        content = req.text
+        index = content.find('百度为您找到相关结果约') + 11
+        content = content[index:]
+        index = content.find('个')
+        count = content[:index].replace(',', '')
+        counts.append(count)
+        #print(choices[i] + " : " + count)
+    output(choices, counts)
+
+def count_base(question,choices):
+    print('\n-- 方法3： 题目搜索结果包含选项词频计数法 --\n')
+    # 请求
+    req = requests.get(url='http://www.baidu.com/s', params={'wd':question})
+    content = req.text
+    #print(content)
+    counts = []
+    print('Question: '+question)
+    if '不是' in question:
+        print('**请注意此题为否定题,选计数最少的**')
+    for i in range(len(choices)):
+        counts.append(content.count(choices[i]))
+        #print(choices[i] + " : " + str(counts[i]))
+    output(choices, counts)
+
+def output(choices, counts):
+    counts = list(map(int, counts))
+    #print(choices, counts)
+
+    # 计数最高
+    index_max = counts.index(max(counts))
+
+    # 计数最少
+    index_min = counts.index(min(counts))
+
+    if index_max == index_min:
+        print(Fore.RED + "高低计数相等此方法失效！" + Fore.RESET)
+        return
+
+    for i in range(len(choices)):
+        print()
+        if i == index_max:
+            # 绿色为计数最高的答案
+            print(Fore.GREEN + "{0} : {1} ".format(choices[i], counts[i]) + Fore.RESET)
+        elif i == index_min:
+            # 红色为计数最低的答案
+            print(Fore.MAGENTA + "{0} : {1}".format(choices[i], counts[i]) + Fore.RESET)
+        else:
+            print("{0} : {1}".format(choices[i], counts[i]))
+
+
+def run_algorithm(al_num, question, choices):
+    if al_num == 0:
+        open_webbrowser(question)
+    elif al_num == 1:
+        open_webbrowser_count(question, choices)
+    elif al_num == 2:
+        count_base(question, choices)
+
+if __name__ == '__main__':
+    question = '新装修的房子通常哪种化学物质含量会比较高?'
+    choices = ['甲醛', '苯', '甲醇']
+    run_algorithm(1, question, choices)
+
