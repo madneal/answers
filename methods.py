f5c5b24c3d461a0e19a8ba70039cc6522c23beab
neal1991 <neal1991@sina.com>
2018-01-25 20:15:47 +0800

    remove unnecessary methods

--- methods.py
+++ methods.py
@@ -1,72 +1,56 @@
 import requests
 import webbrowser
 import urllib.parse
+import re
+from bs4 import BeautifulSoup
+import time
 
 # # 颜色兼容Win 10
 from colorama import init,Fore
 init()
 
 def open_webbrowser(question):
-    webbrowser.open('https://baidu.com/s?wd=' + urllib.parse.quote(question))
+    webbrowser.open('https://www.google.com/search?q=' + urllib.parse.quote(question))
+
 
 def open_webbrowser_count(question,choices):
     print('\n-- 方法2： 题目+选项搜索结果计数法 --\n')
     print('Question: ' + question)
+    is_opsite= False
     if '不是' in question:
         print('**请注意此题为否定题,选计数最少的**')
+        is_opsite = True
 
-    counts = []
+    counts = {}
     for i in range(len(choices)):
         # 请求
-        req = requests.get(url='http://www.baidu.com/s', params={'wd': question + choices[i]})
-        content = req.text
-        index = content.find('百度为您找到相关结果约') + 11
-        content = content[index:]
-        index = content.find('个')
-        count = content[:index].replace(',', '')
-        counts.append(count)
-        #print(choices[i] + " : " + count)
-    output(choices, counts)
+        req = requests.get(url='https://www.google.com/search?q=' + question + choices[i])
+        content = BeautifulSoup(req.content, 'html.parser')
+        result = content.find(id='resultStats').text
+        count = re.sub(r'\D', '', result)
+        count = int(count.replace(',', ''))
+        counts[count] = choices[i]
+    output(counts, is_opsite)
 
-def count_base(question,choices):
-    print('\n-- 方法3： 题目搜索结果包含选项词频计数法 --\n')
-    # 请求
-    req = requests.get(url='http://www.baidu.com/s', params={'wd':question})
-    content = req.text
-    #print(content)
-    counts = []
-    print('Question: '+question)
-    if '不是' in question:
-        print('**请注意此题为否定题,选计数最少的**')
-    for i in range(len(choices)):
-        counts.append(content.count(choices[i]))
-        #print(choices[i] + " : " + str(counts[i]))
-    output(choices, counts)
 
-def output(choices, counts):
-    counts = list(map(int, counts))
-    #print(choices, counts)
-
-    # 计数最高
-    index_max = counts.index(max(counts))
-
-    # 计数最少
-    index_min = counts.index(min(counts))
-
-    if index_max == index_min:
+def output(counts, is_opsite):
+    max_key = max(counts.keys())
+    min_key = min(counts.keys())
+    if max_key == min_key:
         print(Fore.RED + "高低计数相等此方法失效！" + Fore.RESET)
         return
 
-    for i in range(len(choices)):
+    for key in counts:
+        choice = counts[key]
         print()
-        if i == index_max:
-            # 绿色为计数最高的答案
-            print(Fore.GREEN + "{0} : {1} ".format(choices[i], counts[i]) + Fore.RESET)
-        elif i == index_min:
-            # 红色为计数最低的答案
-            print(Fore.MAGENTA + "{0} : {1}".format(choices[i], counts[i]) + Fore.RESET)
+        if is_opsite:
+            compare_key = min_key
         else:
-            print("{0} : {1}".format(choices[i], counts[i]))
+            compare_key = max_key
+        if key == compare_key:
+            print(Fore.RED + '{0} : {1}'.format(choice, key) + Fore.RESET)
+        else:
+            print("{0} : {1}".format(choice, key))
 
 
 def run_algorithm(al_num, question, choices):
@@ -74,11 +58,10 @@ def run_algorithm(al_num, question, choices):
         open_webbrowser(question)
     elif al_num == 1:
         open_webbrowser_count(question, choices)
-    elif al_num == 2:
-        count_base(question, choices)
+
 
 if __name__ == '__main__':
-    question = '新装修的房子通常哪种化学物质含量会比较高?'
+    question = '新装修的房子通常哪种化学物质含量会比较高'
     choices = ['甲醛', '苯', '甲醇']
     run_algorithm(1, question, choices)
 
