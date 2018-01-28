3fa03ddc39d787d8b64cf479fd09949dc1bd15f8
neal1991 <neal1991@sina.com>
2018-01-28 22:08:33 +0800

    refactor run method

--- main.py
+++ main.py
@@ -9,56 +9,33 @@ import re
 def filter_symbol(str):
     return re.sub('[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）《》]+', '', str)
 
+def run():
+    while True:
+        # 截图
+        t = time.clock()
+        screenshot.check_screenshot()
+        img = Image.open("./screenshot.png")
 
-while True:
-    # 截图
-    t = time.clock()
-    screenshot.check_screenshot()
+        config_ = config.load_config()
+        # question, choices = ocr.ocr_img_tess(img, config_)
+        question, choices = ocr.get_question_and_choices(img, config_)
+        question = filter_symbol(question)
 
-    # end_time = time.clock()
-    # print(end_time - t)
+        # 多线程
+        m1 = Thread(methods.run_algorithm(0, question))
+        m2 = Thread(methods.run_algorithm(1, question))
+        m1.start()
+        m2.start()
 
-    img = Image.open("./screenshot.png")
+        end_time3 = time.clock()
+        print('用时: {0}'.format(end_time3 - t))
 
-    # 文字识别,可选 Tesseract 和 Baidu ,请在 config/configure.conf 中进行相应配置
+        go = input('输入回车继续运行,输入 n 回车结束运行: ')
+        if go == 'n':
+            break
+        print('------------------------')
 
-    # ocr_img: 需要分别截取题目和选项区域，使用 Tesseract
-    # ocr_img_tess： 题目和选项一起截，使用 Tesseract
-    # ocr_img_baidu： 题目和选项一起截，使用 baidu ocr，需配置 key
 
-    # question, choices = ocr.ocr_img(img, config)
-    config_ = config.load_config()
-    # question, choices = ocr.ocr_img_tess(img, config_)
-    question, choices = ocr.ocr_img_baidu(img, config_)
-    question = filter_symbol(question)
-
-    # end_time2 = time.clock()
-    # print(end_time2 - end_time)
-
-    # 用不同方法输出结果，取消某个方法在前面加上#
-
-    # # 打开浏览器方法搜索问题
-    # methods.run_algorithm(0, question, choices)
-    # # 将问题与选项一起搜索方法，并获取搜索到的结果数目
-    # methods.run_algorithm(1, question, choices)
-    # # 用选项在问题页面中计数出现词频方法
-    # methods.run_algorithm(2, question, choices)
-
-    # 多线程
-    m1 = Thread(methods.run_algorithm(0, question, choices))
-    m2 = Thread(methods.run_algorithm(1, question, choices))
-    m3 = Thread(methods.run_algorithm(2, question, choices))
-    m1.start()
-    m2.start()
-    m3.start()
-
-    end_time3 = time.clock()
-    print('用时: {0}'.format(end_time3 - t))
-
-    go = input('输入回车继续运行,输入 n 回车结束运行: ')
-    if go == 'n':
-        break
-
-    print('------------------------')
-
+if __name__ == '__main__':
+    run()
 
