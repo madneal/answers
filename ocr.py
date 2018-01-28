9cdfc3f03db097e268e141a027c5808e4fde0488
neal1991 <neal1991@sina.com>
2018-01-28 18:23:35 +0800

    extract image process form ocr_img_baidu method :art:

--- ocr.py
+++ ocr.py
@@ -1,17 +1,9 @@
-# -*- coding: utf-8 -*-
-
-# @Author  : Skye
-# @Time    : 2018/1/9 19:34
-# @desc    :
-
 from PIL import Image
 import pytesseract
-from PIL import ImageFilter
 from aip import AipOcr
 import io
 import time
-import base64
-from colorama import init, Fore
+from colorama import Fore
 import config
 
 
@@ -28,28 +20,8 @@ def binarizing(img, threshold):
     return img
 
 
-# 去除干扰线算法
-def depoint(img):  # input: gray image
-    pixdata = img.load()
-    w, h = img.size
-    for y in range(1, h - 1):
-        for x in range(1, w - 1):
-            count = 0
-            if pixdata[x, y - 1] > 245:
-                count = count + 1
-            if pixdata[x, y + 1] > 245:
-                count = count + 1
-            if pixdata[x - 1, y] > 245:
-                count = count + 1
-            if pixdata[x + 1, y] > 245:
-                count = count + 1
-            if count > 2:
-                pixdata[x, y] = 255
-    return img
-
-
 def get_processed_img(image, region, binary_val):
-    image = image.crop(region[0], region[1], region[2], region[3])
+    image = image.crop((region[0], region[1], region[2], region[3]))
     image_im = image.convert('L')
     image_im = binarizing(image_im, binary_val)
     return image_im
@@ -67,9 +39,19 @@ def ocr_img_tess(img, config_):
         text = text.strip()
     return text_arr
 
+
 def ocr_right_choice(image, config):
     region = config['region']
     choices_region = region['choices_region']
+    img_all = get_processed_img(image, choices_region, 100)
+    img_part = get_processed_img(image, choices_region, 175)
+    if config_['is_debug']:
+        img_all.show()
+        img_part.show()
+    all_choices = ocr_img_baidu(img_all, config_)
+    part_choices = ocr_img_baidu(img_part, config_)
+    right_choice = [choice for choice in all_choices if choice not in part_choices]
+    return ''.join(right_choice)
 
 
 def ocr_img(image, config):
@@ -81,94 +63,11 @@ def ocr_img(image, config):
     # 把图片变成二值图像
     question_im = get_processed_img(image, question_region, 190)
     choices_im = binarizing(image, choices_region, 120)
-    choices_im.show()
+    if config_['is_debug']:
+        choices_im.show()
 
-    # question_im = question_im.convert('1')
-    # choices_im = choices_im.convert('1')
-    # question_im.show()
-    # choices_im.show()
-    # img=depoint(choices_im)
-    # img.show()
-
-    # win环境
-    # tesseract 路径
-    tesseract_config = config_['tesseract']
-    pytesseract.pytesseract.tesseract_cmd =tesseract_config['tesseract_cmd']
-
-    # 语言包目录和参数
-    tessdata_dir_config = tesseract_config['tessdata_dir_config']
-
-    # lang 指定中文简体
-    question = pytesseract.image_to_string(question_im, lang='chi_sim', config=tessdata_dir_config)
-    question = question.replace("\n", "")[2:]
-    # 处理将"一"识别为"_"的问题
-    question = question.replace("_", "一")
-
-    choice = pytesseract.image_to_string(choices_im, lang='chi_sim', config=tessdata_dir_config)
-    # 处理将"一"识别为"_"的问题
-    choices = choice.strip().replace("_", "一").split("\n")
-    choices = [x for x in choices if x != '']
-
-    # 兼容截图设置不对，意外出现问题为两行或三行
-    # if (choices[0].endswith('?')):
-    #     question += choices[0]
-    #     choices.pop(0)
-    # if (choices[1].endswith('?')):
-    #     question += choices[0]
-    #     question += choices[1]
-    #     choices.pop(0)
-    #     choices.pop(0)
-
-    return question, choices
-
-
-def ocr_img_tess(image, config):
-    """只运行一次 Tesseract"""
-    config_region = config['region']
-    combine_region = config_region['combine_region']
-    combine_region = list(map(int, combine_region))
-
-    # 切割题目+选项区域，左上角坐标和右下角坐标,自行测试分辨率
-    region_im = image.crop((combine_region[0], combine_region[1], combine_region[2], combine_region[3]))
-
-    # 转化为灰度图
-    region_im = region_im.convert('L')
-
-    # 把图片变成二值图像
-    region_im = binarizing(region_im, 190)
-
-    # region_im.show()
-
-    # win环境
-    # tesseract 路径
-
-    pytesseract.pytesseract.tesseract_cmd = config.get("tesseract", "tesseract_cmd")
-
-    # 语言包目录和参数
-    tessdata_dir_config = config.get("tesseract", "tessdata_dir_config")
-
-    # lang 指定中文简体
-    region_text = pytesseract.image_to_string(region_im, lang='chi_sim', config=tessdata_dir_config)
-    region_text = region_text.replace("_", "一").split("\n")
-    texts = [x for x in region_text if x != '']
-    # print(texts)
-    if len(texts) > 2:
-        question = texts[0]
-        choices = texts[1:]
-    else:
-        print(Fore.RED + '截图区域设置错误，请重新设置' + Fore.RESET)
-        exit(0)
-
-    # 意外出现问题为两行或三行
-    if choices[0].endswith('?'):
-        question += choices[0]
-        choices.pop(0)
-    elif choices[1].endswith('?'):
-        question += choices[0]
-        question += choices[1]
-        choices.pop(0)
-        choices.pop(0)
-
+    question = ocr_img_tess(question_im, config_)
+    choices = ocr_img_tess(choices_im, config_)
     return question, choices
 
 
@@ -182,21 +81,16 @@ def ocr_img_baidu(image, config_):
 
     client = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
-    global combine_region
-    # 切割题目+选项区域，左上角坐标和右下角坐标,自行测试分辨率
-    region_config = config_['region']
-    combine_region = region_config['combine_region']
-    region_im = image.crop((combine_region[0], combine_region[1], combine_region[2], combine_region[3]))
-    if config_['is_debug']:
-        region_im.show()
-    # 转化为灰度图
-    # region_im = region_im.convert('L')
-
-    # 把图片变成二值图像
-    # region_im = binarizing(region_im, 190)
-    # region_im.show()
+    # region = config_['region']
+    # if type == 0:
+    #     region = region['combine_region']
+    # elif type == 1:
+    #     region = region['question_region']
+    # elif type == 2:
+    #     region = region['choices_region']
+    # region_im = get_processed_img(image, region, binary_val)
     img_byte_arr = io.BytesIO()
-    region_im.save(img_byte_arr, format='PNG')
+    image.save(img_byte_arr, format='PNG')
     image_data = img_byte_arr.getvalue()
     # base64_data = base64.b64encode(image_data)
     response = client.basicGeneral(image_data)
@@ -204,34 +98,38 @@ def ocr_img_baidu(image, config_):
     words_result = response['words_result']
 
     texts = [x['words'] for x in words_result]
-    # print(texts)
-    if len(texts) > 2:
-        question = texts[0]
-        choices = texts[1:]
-        choices = [x.replace(' ', '') for x in choices]
-    else:
-        print(Fore.RED + '截图区域设置错误，请重新设置' + Fore.RESET)
-        exit(0)
-
-    # 处理出现问题为两行或三行
-    if choices[0].endswith('?'):
-        question += choices[0]
-        choices.pop(0)
-    elif choices[1].endswith('?'):
-        question += choices[0]
-        question += choices[1]
-        choices.pop(0)
-        choices.pop(0)
-
-    return question, choices
+    return texts
+    # if type == 0:
+    #     if len(texts) > 2:
+    #         question = texts[0]
+    #         choices = texts[1:]
+    #         choices = [x.replace(' ', '') for x in choices]
+    #     else:
+    #         print(Fore.RED + '截图区域设置错误，请重新设置' + Fore.RESET)
+    #         exit(0)
+    #
+    #     # 处理出现问题为两行或三行
+    #     if choices[0].endswith('?'):
+    #         question += choices[0]
+    #         choices.pop(0)
+    #     elif choices[1].endswith('?'):
+    #         question += choices[0]
+    #         question += choices[1]
+    #         choices.pop(0)
+    #         choices.pop(0)
+    #     return question, choices
+    # else:
+    #     return texts
 
 
 if __name__ == '__main__':
     image = Image.open("img/screenshot.png")
     config_ = config.load_config()
-    time1 = time.time()
-    question, choices = ocr_img(image, config_)
-    print('ocr time is:' + str(time.time() - time1))
-    print("baidu 识别结果:")
-    print(question)
-    print(choices)
+    right_choice = ocr_right_choice(image, config_)
+    print(right_choice)
+    # time1 = time.time()
+    # question, choices = ocr_img(image, config_)
+    # print('ocr time is:' + str(time.time() - time1))
+    # print("baidu 识别结果:")
+    # print(question)
+    # print(choices)
